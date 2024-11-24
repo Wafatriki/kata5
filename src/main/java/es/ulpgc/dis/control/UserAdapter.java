09fc5923c15883ab3fcbda514a970608ec5c8433
Wafatriki <azdadwafa0@gmail.com>
2024-11-24 19:42:28 +0000

    RandomUserResponse implemented

--- src/main/java/es/ulpgc/dis/control/UserAdapter.java
+++ src/main/java/es/ulpgc/dis/control/UserAdapter.java
@@ -2,6 +2,8 @@ package es.ulpgc.dis.control;
 
 import es.ulpgc.dis.model.User;
 
+import java.io.IOException;
+
 public interface UserAdapter<T> {
-    User from(T object);
+    User from(T object) throws IOException;
 }
---
+++ src/main/java/es/ulpgc/dis/control/pojo/RandomUserResponse.java
@@ -0,0 +1,11 @@
+package es.ulpgc.dis.control.pojo;
+
+import java.util.List;
+
+public class RandomUserResponse {
+    private List<RandomUser> result;
+    public List<RandomUser> getResult() {
+        return result;
+    }
+
+}
