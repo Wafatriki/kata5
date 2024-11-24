f2d881f49c0ba7b2aa668ecdb36afe3fe6c5db6c
Wafatriki <azdadwafa0@gmail.com>
2024-11-24 20:10:13 +0000

    RandomUserProvider implemented

--- src/main/java/es/ulpgc/dis/control/RandomUserProvider.java
+++ src/main/java/es/ulpgc/dis/control/RandomUserProvider.java
@@ -1,15 +1,23 @@
 package es.ulpgc.dis.control;
 
+import com.google.gson.Gson;
+import es.ulpgc.dis.control.pojo.RandomUserResponse;
 import es.ulpgc.dis.model.User;
+import org.jsoup.Connection;
 import org.jsoup.Jsoup;
+
+import java.io.IOException;
+import java.util.Random;
 
 public class RandomUserProvider implements UserProvider {
     private static final String URL = "https://randomuser.me/api/";
 
 
     @Override
-    public User newUser() {
-        Jsoup.connect(URL);
+    public User newUser() throws IOException {
+        Connection connect = Jsoup.connect(URL).ignoreContentType(true);
+        RandomUserResponse response = new Gson().fromJson(connect.get().text(), RandomUserResponse.class);
+        return new RandomUserAdapter().from(response.getResult().getFirst());
 
     }
 }
--- src/main/java/es/ulpgc/dis/control/UserProvider.java
+++ src/main/java/es/ulpgc/dis/control/UserProvider.java
@@ -2,6 +2,8 @@ package es.ulpgc.dis.control;
 
 import es.ulpgc.dis.model.User;
 
+import java.io.IOException;
+
 public interface UserProvider {
-    User newUser();
+    User newUser() throws IOException;
 }
